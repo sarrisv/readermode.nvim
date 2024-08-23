2c586c33ade12115165d46d0506d2a37a13a9161
肖健正（家和） <jiahe@bigrule.com>
2024-08-23 12:08:26 +0800

    feat: add filetype config

--- lua/readermode/init.lua
+++ lua/readermode/init.lua
@@ -1,10 +1,17 @@
 local M = {}
 
 ---@class ReaderModeOptions
+---@field enabled? boolean
+---@field keymap? string
+---@field desc? string
+---@field allow_filetypes? string[]
+---@field denied_filetypes? string[]
 local defaults = {
 	enabled = false,
 	keymap = "<leader>R",
 	desc = "Toggle Reader Mode",
+	allow_filetypes = {},
+	denied_filetypes = {},
 }
 
 ---@type ReaderModeOptions
@@ -12,7 +19,7 @@ M.opts = {}
 
 ---@param opts? ReaderModeOptions
 function M.setup(opts)
-	M.opts = opts or defaults
+	M.opts = vim.tbl_deep_extend("force", defaults, opts or {}) or {}
 
 	-- Define ways to toggle readermode
 	vim.api.nvim_create_user_command("ReaderMode", require("readermode").toggle, {})
@@ -24,7 +31,25 @@ function M.setup(opts)
 	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 		group = vim.api.nvim_create_augroup("ReaderMode", { clear = true }),
 		callback = function()
-			if M.opts.enabled then
+			local enabled = M.opts.enabled
+			local allow_filetypes = M.opts.allow_filetypes
+			---@cast allow_filetypes table
+			local denied_filetypes = M.opts.denied_filetypes
+			---@cast denied_filetypes table
+
+			if enabled then
+				local filetype = vim.bo.filetype
+
+				-- denied filetypes contains filetype
+				if vim.tbl_contains(denied_filetypes, filetype) then
+					return
+				end
+
+				-- allow_filetypes is not empty and not contains filetype
+				if vim.tbl_isempty(allow_filetypes) ~= true and vim.tbl_contains(allow_filetypes, filetype) then
+					return
+				end
+
 				local current_line = M.getCurrentLineNumber()
 				if current_line ~= last_line then
 					M.center()
